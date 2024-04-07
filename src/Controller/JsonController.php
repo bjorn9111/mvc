d3dc680023840036fd7f6966604bdea69a3e4579
Björn Horndahl <bjorn9111@gmail.com>
2024-04-07 23:38:23 +0200

    First commit

---
+++ src/Controller/JsonController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JsonController
+{
+    private $number;
+    private $quotes;
+
+    /**
+     * @Route("/api/quote")
+     */
+    #[Route("/api/quote")]
+    public function jsonApi(): Response
+    {
+        $number = random_int(0, 4);
+        
+        $quotes = [
+            'Du kan inte skydda dig själv från sorg utan att skydda dig själv från lycka.',
+            'Vakna. Träna. Se het ut. Kick ass.',
+            'Du behöver inte vara extrem, bara konsekvent.',
+            'Att ursäkta bränner noll kalorier per timme.',
+            'Var inte rädd för att vara nybörjare.',
+        ];
+        
+        
+        $data = [
+            'lucky-message' => $quotes[$number],
+            'lucky-day' => date("Y-m-d"),
+            'lucky-time' => date("H:i:s"),
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        
+        return $response;
+    }
+}
