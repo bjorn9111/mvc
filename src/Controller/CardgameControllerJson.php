6c33471e24715e153b2fa0202c444b3f19578449
Björn Horndahl <bjorn9111@gmail.com>
2024-11-07 11:09:36 +0100

    Add api for cardgame 21 that shows current state

--- src/Controller/CardgameControllerJson.php
+++ src/Controller/CardgameControllerJson.php
@@ -9,6 +9,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CardgameControllerJson
 {
+    #[Route("/api/game", methods: ['GET'])]
+    public function game(
+        SessionInterface $session
+    ): Response {
+        $data = [
+            'game_round' => $session->get("game_round"),
+            'player_wins' => $session->get("player_wins"),
+            'bank_wins' => $session->get("bank_wins")
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+
     #[Route("/api/deck", methods: ['GET'])]
     public function deck(): Response
     {
