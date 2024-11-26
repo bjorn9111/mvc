a2dabadf51e8a175b6f962b2af74495b5731a0d9
Björn Horndahl <bjorn9111@gmail.com>
2024-11-26 15:52:50 +0100

    Further improve CardGraphic

--- src/Cardgame/CardGraphic.php
+++ src/Cardgame/CardGraphic.php
@@ -15,31 +15,43 @@ class CardGraphic extends Card
 
     public function getAsString(): string
     {
-        if ('Spades' === $this->suit) {
-            if ($this->value > 11) {
-                return $this->representation = mb_chr((127137 + $this->value), "UTF-8");
+        $suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+        $mdChrStart = 127136;
+        foreach ($suits as $suit) {
+            if ($suit === $this->suit) {
+                if ($this->value > 11) {
+                    return $this->representation = mb_chr((($mdChrStart + 1) + $this->value), "UTF-8");
+                }
+                return $this->representation = mb_chr(($mdChrStart + $this->value), "UTF-8");
             }
-            return $this->representation = mb_chr((127136 + $this->value), "UTF-8");
+            $mdChrStart += 16;
         }
-
-        if ('Hearts' === $this->suit) {
-            if ($this->value > 11) {
-                return $this->representation = mb_chr((127153 + $this->value), "UTF-8");
-            }
-            return $this->representation = mb_chr((127152 + $this->value), "UTF-8");
-        }
-
-        if ('Diamonds' === $this->suit) {
-            if ($this->value > 11) {
-                return $this->representation = mb_chr((127169 + $this->value), "UTF-8");
-            }
-            return $this->representation = mb_chr((127168 + $this->value), "UTF-8");
-        }
-
-        if ($this->value > 11) {
-            return $this->representation = mb_chr((127185 + $this->value), "UTF-8");
-        }
-        $this->representation = mb_chr((127184 + $this->value), "UTF-8");
         return $this->representation;
+        // if ('Spades' === $this->suit) {
+        //     if ($this->value > 11) {
+        //         return $this->representation = mb_chr((127137 + $this->value), "UTF-8");
+        //     }
+        //     return $this->representation = mb_chr((127136 + $this->value), "UTF-8");
+        // }
+
+        // if ('Hearts' === $this->suit) {
+        //     if ($this->value > 11) {
+        //         return $this->representation = mb_chr((127153 + $this->value), "UTF-8");
+        //     }
+        //     return $this->representation = mb_chr((127152 + $this->value), "UTF-8");
+        // }
+
+        // if ('Diamonds' === $this->suit) {
+        //     if ($this->value > 11) {
+        //         return $this->representation = mb_chr((127169 + $this->value), "UTF-8");
+        //     }
+        //     return $this->representation = mb_chr((127168 + $this->value), "UTF-8");
+        // }
+
+        // if ($this->value > 11) {
+        //     return $this->representation = mb_chr((127185 + $this->value), "UTF-8");
+        // }
+        // $this->representation = mb_chr((127184 + $this->value), "UTF-8");
+        // return $this->representation;
     }
 }
